784c26b75fb18996f4247972d89679e32848c8d6
Garcia, John Francis <john.francis.garcia@accenture.com>
2024-01-03 13:21:16 +0800

    fixed bugs

--- src/components/profile/ProfileExperience.js
+++ src/components/profile/ProfileExperience.js
@@ -8,7 +8,7 @@ const ProfileExperience = ({
 	return (
 		<>
 			<div>
-				<h3 class="text-dark">Microsoft</h3>
+				<h3 class="text-dark">{company}</h3>
 				<p>
 					<Moment format="YYYY/MM/DD">{from}</Moment> -{" "}
 					{current ? " Now" : <Moment format="YYYY/MM/DD">{to}</Moment> }
